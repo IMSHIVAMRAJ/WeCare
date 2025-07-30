2af30984e31749d1907078cddf0d9dfd6939e369
IMSHIVAMRAJ <sraj2004bxr@gmail.com>
2025-07-30 15:50:25 +0530

    c

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-// import adminRoutes from "./routes/admin.routes.js";
-// import authRoutes from "./routes/auth.routes.js";
-// import serviceRoutes from "./routes/service.routes.js";
-// import bookingRoutes from "./routes/booking.routes.js";
-// import userRoutes from "./routes/user.routes.js";
-// import beauticianRoutes from "./routes/beautician.routes.js";
-// import locationRoutes from "./routes/locationRoutes.js";
-// import adminAuthRoutes from "./routes/adminAuth.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import serviceRoutes from "./routes/service.routes.js";
+import bookingRoutes from "./routes/booking.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import beauticianRoutes from "./routes/beautician.routes.js";
+import locationRoutes from "./routes/locationRoutes.js";
+import adminAuthRoutes from "./routes/adminAuth.routes.js";
 
 dotenv.config();
 const app = express();
@@ -31,14 +31,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 // API Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/services", serviceRoutes);
-// app.use("/api/booking", bookingRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/beautician", beauticianRoutes);
-// app.use("/api/admin-auth", adminAuthRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/locations", locationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/booking", bookingRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/beautician", beauticianRoutes);
+app.use("/api/admin-auth", adminAuthRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/locations", locationRoutes);
 
 // Database Connection and Server Start
 mongoose
